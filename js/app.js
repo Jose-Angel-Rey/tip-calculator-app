20ce0e7c1c928e957e0bc6816b9f9d015908384d
Jose Angel Rey <dev.joseangel.rey@gmail.com>
2021-07-10 15:11:16 -0400

    Update

--- js/app.js
+++ js/app.js
@@ -1,27 +1,73 @@
-const billInitial = document.getElementById("bill");
-const tipPercentage = document.getElementById("tip");
-const people = document.getElementById("people");
+"use strict";
+// Botones
+const tipButtons = document.querySelectorAll(".btn-tip");
+const customTip = document.getElementById("custom-tip");
 
+// Inputs
+const inputPeople = document.getElementById("people");
+const inputBill = document.getElementById("bill");
+
+let tipValue = 1;
+let billValue;
+let peopleValue;
+
+// Bill input
+inputBill.addEventListener("input", () => {
+    billValue = inputValidation(inputBill);
+});
+
+// People input
+inputPeople.addEventListener("input", () => {
+    peopleValue = inputValidation(inputPeople);
+});
+
+function inputValidation(input) {
+    let inputWarning = input.previousElementSibling
+    if (input.value <= 0) {
+        inputWarning.classList.add("input__warning--active");
+        input.style.borderColor = "#c91e1e;"
+    } else if (input.value > 0 && inputWarning.classList.contains("input__warning--active")) inputWarning.classList.remove("input__warning--active");
+
+    if (input.value.length > input.maxLength) input.value = input.value.slice(0, input.maxLength);
+
+    return input.value;
+}
+
+// Totales
 let totalAmount = document.querySelector(".total__amount-per-person");
 let totalTip = document.querySelector(".tip-amount__amount-per-person");
 
-// console.log(totalTip)
+// -----------------------------------------------------------------
 
-let person = 100;
-amountPerPerson(100, 50);
-console.log((total = `$ ${person.toFixed(5)}`));
-
-// console.log(tipPercentage);
-console.log(people);
-
-// if (billInitial.valu) {
-// }
-
-billInitial.addEventListener("input", () => {
-  console.log(billInitial.value);
+// Capturar el valor de los botones para el porcentaje de la propina
+tipButtons.forEach((tipButton) => {
+    tipButton.addEventListener("click", () => {
+        if (!tipButton.classList.contains("btn-tip--active")) {
+            tipButtons.forEach((button) => {
+                button.classList.remove("btn-tip--active");
+            });
+            tipButton.classList.add("btn-tip--active");
+            tipValue = tipButton.getAttribute("data-value");
+        } else {
+            tipButton.classList.remove("btn-tip--active");
+            tipValue = 1;
+        }
+    });
 });
 
-function amountPerPerson(tip, amount) {
-  totalTip.textContent = `$${tip.toFixed(2)}`;
-  totalAmount.textContent = `$${amount.toFixed(2)}`;
-}
+// Valor Custom
+customTip.addEventListener("click", () => {
+    // Remueve el enfasis de los otros botones
+    tipButtons.forEach((tipButton) => {
+        if (tipButton.classList.contains("btn-tip--active")) {
+            tipButton.classList.remove("btn-tip--active");
+            tipValue = 1;
+        }
+    });
+});
+
+// Obtener el valor custom
+customTip.addEventListener("input", () => {
+    tipValue = inputValidation(customTip)
+});
+// -----------------------------------------------------------------
